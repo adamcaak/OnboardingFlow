8e14bc2d4ebff1beb06a74f2abd482bc4a8481b8
adamcaak <ladamczak1997@gmail.com>
2025-06-22 11:49:51 +0200

    Update contentview and add tabview

--- OnboardingFlow/FeaturesPage.swift
+++ OnboardingFlow/FeaturesPage.swift
@@ -9,12 +9,15 @@ import SwiftUI
 
 struct FeaturesPage: View {
     var body: some View {
-        Text("Features")
-            .font(.title)
-            .fontWeight(.semibold)
-            .padding(.bottom)
-        FeatureCard(iconName: "person.2.crop.square.stack.fill", description: "A multiline description about a feature paired with the image on the left.")
-        FeatureCard(iconName: "quote.bubble.fill", description: "Short summary")
+        VStack {
+            Text("Features")
+                .font(.title)
+                .fontWeight(.semibold)
+                .padding(.bottom)
+            FeatureCard(iconName: "person.2.crop.square.stack.fill", description: "A multiline description about a feature paired with the image on the left.")
+            FeatureCard(iconName: "quote.bubble.fill", description: "Short summary")
+        }
+        .padding()
     }
 }
 

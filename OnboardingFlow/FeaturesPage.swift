595f53cea411840dbc41d3adbdee909ba3020324
adamcaak <ladamczak1997@gmail.com>
2025-06-22 11:40:55 +0200

    Add property frame, padding, spacer

--- OnboardingFlow/FeaturesPage.swift
+++ OnboardingFlow/FeaturesPage.swift
@@ -14,6 +14,7 @@ struct FeaturesPage: View {
             .fontWeight(.semibold)
             .padding(.bottom)
         FeatureCard(iconName: "person.2.crop.square.stack.fill", description: "A multiline description about a feature paired with the image on the left.")
+        FeatureCard(iconName: "quote.bubble.fill", description: "Short summary")
     }
 }
 

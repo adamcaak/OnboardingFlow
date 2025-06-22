0fa6767fd03b88a3f71c7c18329a18c969d06839
adamcaak <ladamczak1997@gmail.com>
2025-06-22 12:08:24 +0200

    Update style for views

--- OnboardingFlow/ContentView.swift
+++ OnboardingFlow/ContentView.swift
@@ -7,13 +7,20 @@
 
 import SwiftUI
 
+let gradient: [Color] = [
+    .gradientTop,
+    .gradientBottom
+]
+
 struct ContentView: View {
     var body: some View {
         TabView {
             WelcomePage()
             FeaturesPage()
         }
+        .background(Gradient(colors: gradient))
         .tabViewStyle(.page)
+        .foregroundStyle(.white)
     }
 }
 

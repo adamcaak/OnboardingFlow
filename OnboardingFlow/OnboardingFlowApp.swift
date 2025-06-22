2190f33da45350b9c038d4f2e435710191e95e21
adamcaak <ladamczak1997@gmail.com>
2025-06-22 10:47:32 +0200

    Initial Commit

---
+++ OnboardingFlow/OnboardingFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingFlowApp.swift
+//  OnboardingFlow
+//
+//  Created by Łukasz Adamczak on 22/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
